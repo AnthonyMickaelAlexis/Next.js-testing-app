abd54306a4c6fd59c9259552a0e74ad9a9332d40
Anthony-Mickaël Alexis <amalexis@etelm-2021.local>
2022-09-14 11:08:02 +0200

    pushing base of the app on github

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f594d523ad2bc9eb"; }
+/******/ 		__webpack_require__.h = function() { return "d3a9c11b203dacf1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
