6be37f696fd1e200b0ccfe00ab26deb1fbd030fe
Anthony-Mickaël Alexis <amalexis@etelm-2021.local>
2022-09-14 11:16:48 +0200

    push the current version of the project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d3a9c11b203dacf1"; }
+/******/ 		__webpack_require__.h = function() { return "11deb9b31ba6b77e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
