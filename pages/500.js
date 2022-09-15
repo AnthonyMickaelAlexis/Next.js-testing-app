fea7379641e088d5f9d26d7cd812787069543581
Anthony-Mickaël Alexis <amalexis@etelm-2021.local>
2022-09-15 14:56:52 +0200

    fixing come conflicts to push on vercel adding <h1> to 500.js

--- pages/500.js
+++ pages/500.js
@@ -1,6 +1,6 @@
 export default function Error() {
     return (
         <p>
-            <statusCode>500 - Server-side error occurred</h1>)
+            <h1><statusCode>500 - Server-side error occurred</h1>)
         </p>
 }
